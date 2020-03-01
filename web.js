ce02bb047863a29cb9af8d42f5fe84eaf1eba22d
usage <kyr9389@naver.com>
2020-03-01 14:37:17 +0000

    [UPDATE] 8th pirate set item added
    Signed-off-by: usage <kyr9389@naver.com>

---
+++ web.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+var item = require('./routes/item');
+var iteminfo = require('./routes/iteminfo');
+var guide = require('./routes/guide');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// 메소드와 관계없이 아래 url로 시작하는건 route 폴더의 모듈에서 처리하기로 함
+app.use('/', index);
+app.use('/item', item);
+app.use('/iteminfo', iteminfo);
+app.use('/guide', guide);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+	/*
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  */
+  res.render('error');
+});
+
+app.listen(process.env.PORT || 3000, () => console.log('Example app listening on port 3000!'));
+
+module.exports = app;
