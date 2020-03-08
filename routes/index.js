7ce30de761678c348a01b6dccf089354371986ac
usage <kyr9389@naver.com>
2020-03-08 13:35:59 +0000

    [MODIFY] console log clear
    Signed-off-by: usage <kyr9389@naver.com>

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  console.log("[정보] 유저가 접속하였습니다.");
   res.render('index', { title: 'Express' });
 });
 
